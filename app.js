7146fb64fd79ef138ad16620c09013fc58c6bb6b
Emil Lindström <lindstromemil86@gmail.com>
2021-08-20 06:34:49 +0200

    trying to get clear all function to work

--- app.js
+++ app.js
@@ -3,36 +3,47 @@ const buttonsOperand = document.querySelectorAll('.operand');
 const buttonClearAll = document.querySelector('.clear');
 const buttonDelete = document.querySelector('.delete');
 const buttonEquals = document.querySelector('.equals');
+const paragraph = document.getElementById('p');
 
 let num = '';
 let numTwo = '';
 let operator = '';
 
 
+// loops through all digit buttons and onclick, display it on screen whilte creating a p tag.
+// Paring num to an int. 
 buttonsDigit.forEach(button => {
-    const paragraph = document.getElementById('p');
     button.addEventListener('click', (e) => {
     const text = document.createTextNode(e.target.innerText);
     paragraph.appendChild(text);
-    console.log(paragraph);
     num += e.target.innerText
     num = parseInt(num);
+    console.log(num)
     })
 
 
 })
 
-
-
+// loops through operators, adding them to screen
 
 buttonsOperand.forEach(button => {
     button.addEventListener('click', (e) => {
-        console.log(e.target.innerText)
+        const text = document.createTextNode(e.target.innerText);
+        paragraph.appendChild(text);
+        
     })
 })
-
+// clear all numbers and operators stored in variables and on-screen
 buttonClearAll.addEventListener('click', (e) => {
     console.log(e.target.innerText)
+    num = '';
+    numTwo = '';
+    operator = '';
+    const text = document.createTextNode(e.target.innerText);
+    paragraph.remove(text); // Fix This
+  
+    
+
 })
 buttonDelete.addEventListener('click', (e) => {
     console.log(e.target.innerText)
