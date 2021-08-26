fa0a600fa35fe5aa7af7848f024539ecf3115a76
Emil Lindström <lindstromemil86@gmail.com>
2021-08-26 14:03:31 +0200

    finished calculator basic function

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // TODO
 
-// make operator replace when button pressed
-// make new variable with = results
-// make it not break when using delete button +++
-// make only one function out of operate instead of several callbacks
-
+// Make delete/backspace button work
+// add floating point numbers
+// add maximum numbers so it does not break display.
+// add keyboard support
 
 const buttonsDigit = document.querySelectorAll('.digit');
 const buttonsOperator = document.querySelectorAll('.operand');
@@ -13,15 +12,11 @@ const buttonDelete = document.querySelector('.delete');
 const buttonEquals = document.querySelector('.equals');
 const paragraph = document.getElementById('p');
 
-
 let num = '';
 let numTwo = '';
 let operator = '';
 
-
-
 // loops through all digit buttons and onclick, display it on screen whilte creating a p tag.
-
 // adding to numTwo instead of num if operator is chosen
 
 buttonsDigit.forEach(button => {
@@ -30,46 +25,43 @@ buttonsDigit.forEach(button => {
             const text = document.createTextNode(e.target.innerText);
             paragraph.appendChild(text);
             num += e.target.innerText;
-             num = parseInt(num);
-            
+             num = parseInt(num);   
         }
         else if (operator !== '') {
             const text = document.createTextNode(e.target.innerText);
             paragraph.appendChild(text);
             numTwo += e.target.innerText;
-            numTwo = parseInt(numTwo);
-             
+            numTwo = parseInt(numTwo);  
         }
-        
-
      })
 })
 
-// loops through operators, adding them to screen and stores in variable. Only if empty.
+// loops through operators, adding them to screen and stores in variable.
 
 buttonsOperator.forEach(button => {
     button.addEventListener('click', (e) => {
-        if (operator === "") {
+        if (operator === '') {
             const text = document.createTextNode(e.target.innerText);
             paragraph.appendChild(text);
-            operator += e.target.value;
-            console.log(e.target.value)
-            
+            operator += e.target.value;           
         }
-        else if (operator !== '' && num == ''){
-            operator = '';
-            const text = document.createTextNode(e.target.innerText);
-            paragraph.appendChild(text);
-            operator += e.target.value;
-            console.log(e.target.value)
-            paragraph.innerText = operator;
-        }
+         else if (operator !== '' && num == ''){
+             operator = '';
+             operator += e.target.value;
+             paragraph.innerText = e.target.innerText  
+         }
+        else if (operator !=='' && numTwo !==''){
+             paragraph.innerText = `= ${operate(operator, num, numTwo)}${e.target.innerText}`;
+             num = operate(operator, num, numTwo);
+             numTwo = '';
+             operator = '';
+             operator += e.target.value;
+        } 
     })
 })
 
 // clear all numbers and operators stored in variables and on-screen
 buttonClearAll.addEventListener('click', (e) => {
-    console.log(e.target.innerText)
     num = '';
     numTwo = '';
     operator = '';
@@ -89,8 +81,11 @@ buttonDelete.addEventListener('click', (e) => {
 
 // Equals button calling operate function and changing innertext.
 buttonEquals.addEventListener('click', (e) => {
-    console.log(operate(operator, num, numTwo));
     paragraph.innerText= `= ${operate(operator, num, numTwo)}`;
+    num = operate(operator, num, numTwo);
+    numTwo = '';
+    operator = '';
+
 })
 
 // Single operation functions
@@ -109,7 +104,7 @@ function multiply(num, numTwo) {
 function divide(num, numTwo) {
     return num / numTwo;
 }
-// Operate function. Maybe integrate the above functions into this.
+// Operate function
 function operate(operator, num, numTwo){
     if (operator == '+') {
         return add(num, numTwo);
@@ -123,7 +118,7 @@ function operate(operator, num, numTwo){
     if (operator == '/') {
         return divide(num, numTwo);
     } else {
-        return false;
+        return paragraph.innerText = "Please redo calculation (CA)";
     }
 };
 
